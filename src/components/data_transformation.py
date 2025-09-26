126db3a858dcba609bef6917efbf011a1ea81094
vamsintech <vamsi44p@gmail.com>
2025-09-26 09:44:51 -0500

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -93,6 +93,11 @@ class DataTransformation:
 
             save_object( file_path = self.data_transformation_config.preprocessor_obj_file_path,
                         obj = preprocessing_obj)
+            
+            return(
+                train_arr, test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
 
 
         except Exception as e:
